2bdbd14ecc6d51b0aa6e97747db6d951a1e632a9
henrysalim <henrysalim22@gmail.com>
2025-10-15 20:55:13 +0700

    fix button color in article detail; added dummy login and register

--- app/src/main/java/com/example/midtermexam/MainActivity.kt
+++ app/src/main/java/com/example/midtermexam/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.midtermexam
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -11,23 +12,29 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+    // inisialisasi variabel untuk kemudian diinisasi saat create (onCreate())
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var bottomNavView: BottomNavigationView
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // 1. Inisialisasi semua komponen UI
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        bottomNavView = findViewById(R.id.bottom_navigation)
+        toolbar = findViewById(R.id.toolbar)
 
         // 2. Jadikan Toolbar sebagai Action Bar utama aplikasi
         setSupportActionBar(toolbar)
 
         // 3. Konfigurasi halaman mana yang dianggap "level atas" (tidak punya tombol kembali)
         // Ganti ID ini sesuai dengan ID fragment di menu bawah Anda
-        val appBarConfiguration = AppBarConfiguration(
+        appBarConfiguration = AppBarConfiguration(
             setOf(R.id.homeFragment, R.id.articleFragment)
         )
 
@@ -35,16 +42,23 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavView.setupWithNavController(navController)
 
+        // atur visibilitas support action bar dan bottom nav view
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.profileFragment -> {
-                    toolbar.visibility = View.GONE
-                }
-                else -> {
-                    toolbar.visibility = View.VISIBLE
-                }
+            // jika di halaman detail article, tampilkan tombol back pada support action bar
+            if (destination.id == R.id.articleDetailFragment) {
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+            // selain di halaman detail article, jangan tampilkan tombol back
+            } else {
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
             }
+
+            // jika sedang di halaman login/register, jangan tampilkan bottom nav view
+            if (destination.id == R.id.loginFragment || destination.id == R.id.registerFragment) {
+                bottomNavView.visibility = View.GONE
+            } else bottomNavView.visibility = View.VISIBLE
         }
+
     }
 
     /**
@@ -52,8 +66,6 @@ class MainActivity : AppCompatActivity() {
      * Ini akan menangani aksi klik pada panah kembali.
      */
     override fun onSupportNavigateUp(): Boolean {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/midtermexam/ui/auth/RegisterFragment.kt
@@ -0,0 +1,87 @@
+package com.example.midtermexam.ui.auth
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.example.midtermexam.R
+import com.example.midtermexam.model.AuthViewModel
+import com.google.android.material.textfield.TextInputEditText
+
+class RegisterFragment : Fragment() {
+    // buat variabel untuk menampung elemen-elemen
+    private lateinit var fullName: TextInputEditText
+    private lateinit var username: TextInputEditText
+    private lateinit var email: TextInputEditText
+    private lateinit var password: TextInputEditText
+    private lateinit var registerBtn: Button
+
+    // buat variabel dari AuthViewModel
+    private val authViewModel: AuthViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // cari elemen berdasarkan ID nya
+        registerBtn = view.findViewById(R.id.registerBtn)
+        fullName = view.findViewById(R.id.editTextFullName)
+        username = view.findViewById(R.id.editTextUsername)
+        email = view.findViewById(R.id.editTextEmail)
+        password = view.findViewById(R.id.editTextPassword)
+
+        // tambahkan on click listener ke button register
+        registerBtn.setOnClickListener { register() }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    private fun register() {
+        // ambil nilai dari input
+        val fullName = fullName.text.toString()
+        val username = username.text.toString()
+        val email = email.text.toString()
+        val password = password.text.toString()
+
+        // jika semua input tidak null
+        if (fullName.isNotBlank() && username.isNotBlank() && email.isNotBlank() && password.isNotBlank()) {
+            if(password.length < 8) Toast.makeText(requireContext(), "Password min. 8 karakter", Toast.LENGTH_SHORT).show()
+            else {
+                try {
+                    // isi data pada AuthViewModel dengan data tsb
+                    authViewModel.setFullName(fullName)
+                    authViewModel.setUsername(username)
+                    authViewModel.setEmail(email)
+                    authViewModel.setPassword(password)
+                    authViewModel.setSuccessMessage("Registrasi berhasil!")
+
+                    // navigate ke halaman login
+                    findNavController().navigate(R.id.action_RegisterFragment_to_LoginFragment)
+
+                    // jika ada error...
+                } catch (e: Exception) {
+                    // tampilkan error nya via Toast
+                    Toast.makeText(requireContext(), "An error has ocurred: ${e.message}", Toast.LENGTH_SHORT).show()
+                    Log.d("ERR_REGISTER", e.message.toString())
+                }
+            }
+
+            // jika ada input yang masih kosong...
+        } else {
+            // tampilkan pesan
+            Toast.makeText(requireContext(), "Semua input harus diisi", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
